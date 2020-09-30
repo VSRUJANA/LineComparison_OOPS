111cf322262166a973e2c135044490e06c009b33
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 12:43:30 +0530

    [srujana] Add . Program to calculate length of the line added

--- Line_Comparison_OOPS/Program.cs
+++ Line_Comparison_OOPS/Program.cs
@@ -6,7 +6,18 @@ namespace Line_Comparison_OOPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Program!");
+            Console.WriteLine("Welcome to Line Comparison Program! ");
+            double[] x = new double[2];
+            double[] y = new double[2];
+            Random random = new Random();
+            for (int index = 0; index < 2; index++)
+            {
+                x[index] = random.Next(-100, 100);
+                y[index] = random.Next(-100, 100);
+            }
+            Console.WriteLine("Coordinates of two end points of the line : ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+            double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
+            Console.WriteLine("Length of the line: " + Math.Round(length, 2));
         }
     }
 }
