b72ebad28a873c08da16ee37c03be179512d8a6a
VSRUJANA <srujana.valavala@gmail.com>
2020-09-30 13:00:57 +0530

    [srujana] Add . Program to check whether two lines are equal is added

--- Line_Comparison_OOPS/Program.cs
+++ Line_Comparison_OOPS/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
 namespace Line_Comparison_OOPS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Program! ");
+            Console.WriteLine("Welcome to Line Comparison Computation Program!");
+            Console.Write("The coordinates of end points of Line 1 are ");
+            double lengthOfLine1 = LengthOfLine();
+            Console.WriteLine("Length of line 1: " + lengthOfLine1);
+            Console.Write("The coordinates of end points of Line 2 are ");
+            double lengthOfLine2 = LengthOfLine();
+            Console.WriteLine("Length of line 2: " + lengthOfLine2);
+            if (lengthOfLine1 == lengthOfLine2)
+                Console.WriteLine("Both the lines are equal");
+            else
+                Console.WriteLine("Both the lines are unequal");
+        }
+        static double LengthOfLine()
+        {
             double[] x = new double[2];
             double[] y = new double[2];
             Random random = new Random();
@@ -15,9 +27,9 @@ namespace Line_Comparison_OOPS
                 x[index] = random.Next(-100, 100);
                 y[index] = random.Next(-100, 100);
             }
-            Console.WriteLine("Coordinates of two end points of the line : ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+            Console.WriteLine(" ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
             double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
-            Console.WriteLine("Length of the line: " + Math.Round(length, 2));
+            return Math.Round(length, 2);
         }
     }
 }
